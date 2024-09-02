b9bbfd35804ba0b15791d9dccf3ce6a0eb0efc98
SkAppCoding <skappcoding+github@gmail.com>
2024-09-02 14:50:17 +0200

    started with Retrofit and created api and repository

---
+++ app/src/main/java/com/example/readerapp/repository/BookRepository.kt
@@ -0,0 +1,41 @@
+package com.example.readerapp.repository
+
+import android.provider.ContactsContract.Data
+import com.example.readerapp.data.DataOrException
+import com.example.readerapp.model.Book
+import com.example.readerapp.model.Item
+import com.example.readerapp.network.BooksApi
+import javax.inject.Inject
+
+class BookRepository @Inject constructor(private val api: BooksApi) {
+
+    private val dataOrException = DataOrException<List<Item>, Boolean, Exception>()
+    private val bookInfoDataOrException = DataOrException<Item, Boolean, Exception>()
+
+    suspend fun getBooks(searchQuery: String): DataOrException<List<Item>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllBooks(searchQuery).items
+            if (dataOrException.data!!.isNotEmpty()) dataOrException.loading = false
+        } catch (ex: Exception) {
+            dataOrException.ex = ex
+        }
+
+        return dataOrException
+    }
+
+    suspend fun getBookInfo(bookId: String) : DataOrException<Item, Boolean, Exception>{
+        val response = try {
+            bookInfoDataOrException.loading = true
+            api.getBookInfo(bookId)
+
+            if (bookInfoDataOrException.data.toString().isNotEmpty()) bookInfoDataOrException.loading = false
+            else {}
+
+        } catch (ex: Exception) {
+            dataOrException.ex = ex
+        }
+        return bookInfoDataOrException
+    }
+
+}
